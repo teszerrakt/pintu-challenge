80ee511be9be7d14d7a0360e5396cd093b908996
Zaky Syihab Hatmoko <zaky.syihab@gmail.com>
2023-06-28 04:04:23 +0700

    feat: add ssr on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,22 @@
 import HomeScreen from 'src/screens/Home'
+import { getPayload } from 'src/pages/api/latestPrice'
+
+export async function getServerSideProps() {
+  try {
+    const initialData = await getPayload()
+
+    return {
+      props: {
+        initialData,
+      },
+    }
+  } catch (error) {
+    return {
+      props: {
+        initialData: [],
+      },
+    }
+  }
+}
 
 export default HomeScreen
