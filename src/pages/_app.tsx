f2cb7bad2611ace4f37f52d03a965e57b696b68d
Zaky Syihab Hatmoko <zaky.syihab@gmail.com>
2023-06-30 02:31:09 +0700

    fix: remove top padding

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,7 +10,7 @@ const queryClient = new QueryClient()
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className={`${inter.className} mx-auto md:p-5 max-w-7xl`}>
+      <div className={`${inter.className} mx-auto md:p-5 md:pt-0 max-w-7xl`}>
         <Component {...pageProps} />
       </div>
     </QueryClientProvider>
